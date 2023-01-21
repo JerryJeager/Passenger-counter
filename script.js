8a959fb6c357bb34bb11b1636b5ebcdcd0d0ba4b
Amadi Jerry <amadijerry310@gmail.com>
2023-01-21 09:48:54 +0100

    enabled calculation of cashout

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const saveBtn = document.querySelector(".save-btn")
 const entries = document.querySelector(".entries")
 let passengerNum = 0
 let newPassengerValue = 0
+let passengerNumArr = []
+let totalCashArr = []
+let totalPassenger = 0
+let totalCash = 0
 
 menu.addEventListener("click", () => {
     profitDetails.classList.toggle("hide-profit-details")
@@ -30,10 +34,20 @@ saveBtn.addEventListener("click", () => {
     if(newPassengerValue == 0 || !newPassengerValue)
         return
     entries.innerHTML += `<div class="people-entered-num entry">${passengerNum}</div>`
+    passengerNumArr.push(passengerNum)
+    totalCashArr.push(passengerNum * 100)
+    calcEntries()
     passengerNum = 0
     displayPassengerNum()
 })
 
 function displayPassengerNum() {
     passengerNumDisplay.textContent = `${passengerNum}`
+}
+
+function calcEntries(){
+    totalPassenger = passengerNumArr.reduce((a, b) => a + b)
+    totalCash = totalCashArr.reduce((a, b) => a + b)
+    document.querySelector(".total-entries-info").textContent = `${totalPassenger} Passenger(s)`
+    document.querySelector(".cashout").innerHTML = `Cashout: <span><i class="fa-solid fa-naira-sign">${totalCash}`
 }
