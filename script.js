af72dc690e7e4f6908212a20f8f17fa65a6066ec
Amadi Jerry <amadijerry310@gmail.com>
2023-01-21 06:26:52 +0100

    added increment and decrement of passengers

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const menu = document.querySelector(".menu")
 const profitDetails = document.querySelector(".profit-details")
+const passengerNumDisplay = document.querySelector(".people-entered-num")
+const addBtn = document.querySelector(".add-btn")
+const minusBtn = document.querySelector(".minus-btn")
+let passengerNum = 0
 
 menu.addEventListener("click", () => {
     profitDetails.classList.toggle("hide-profit-details")
+})
+
+addBtn.addEventListener("click", () => {
+    passengerNumDisplay.textContent = `${++passengerNum}`
+})
+
+minusBtn.addEventListener("click", () => {
+    if(passengerNum < 0)
+        return
+    passengerNumDisplay.textContent = `${passengerNum--}`
 })
