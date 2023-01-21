84298f393e2245f7049813dfe4ebd0c0b8594806
Amadi Jerry <amadijerry310@gmail.com>
2023-01-21 08:19:44 +0100

    enabled passenger counting

--- script.js
+++ script.js
@@ -3,18 +3,37 @@ const profitDetails = document.querySelector(".profit-details")
 const passengerNumDisplay = document.querySelector(".people-entered-num")
 const addBtn = document.querySelector(".add-btn")
 const minusBtn = document.querySelector(".minus-btn")
+const saveBtn = document.querySelector(".save-btn")
+const entries = document.querySelector(".entries")
 let passengerNum = 0
+let newPassengerValue = 0
 
 menu.addEventListener("click", () => {
     profitDetails.classList.toggle("hide-profit-details")
 })
 
 addBtn.addEventListener("click", () => {
-    passengerNumDisplay.textContent = `${++passengerNum}`
+    ++passengerNum
+    displayPassengerNum()
 })
 
 minusBtn.addEventListener("click", () => {
-    if(passengerNum < 0)
+    if (passengerNum <= 0)
         return
-    passengerNumDisplay.textContent = `${passengerNum--}`
+    passengerNum--
+    displayPassengerNum()
 })
+
+saveBtn.addEventListener("click", () => {
+    // alert("Entry saved.")
+    newPassengerValue = passengerNumDisplay.textContent
+    if(newPassengerValue == 0 || !newPassengerValue)
+        return
+    entries.innerHTML += `<div class="people-entered-num entry">${passengerNum}</div>`
+    passengerNum = 0
+    displayPassengerNum()
+})
+
+function displayPassengerNum() {
+    passengerNumDisplay.textContent = `${passengerNum}`
+}
